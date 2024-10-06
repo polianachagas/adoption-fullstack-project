a2fcea3dea6121ebacf5b65fecc7b22da16614e4
polianachagas <polianarc934@gmail.com>
2024-10-06 19:03:53 -0300

    model class and connection with DB

--- src/main/java/com/polianachagas/adoption_project/model/Animal.java
+++ src/main/java/com/polianachagas/adoption_project/model/Animal.java
@@ -1,5 +1,43 @@
 package com.polianachagas.adoption_project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Animal {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Integer age;
+	private String image;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+	
 }
--- src/main/java/com/polianachagas/adoption_project/repository/AnimalRepository.java
+++ src/main/java/com/polianachagas/adoption_project/repository/AnimalRepository.java
@@ -1,5 +1,9 @@
 package com.polianachagas.adoption_project.repository;
 
-public class AnimalRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.polianachagas.adoption_project.model.Animal;
+
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
 
 }
