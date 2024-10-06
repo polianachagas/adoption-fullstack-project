cece975052515164850a2fd42cc448db811e21a9
polianachagas <polianarc934@gmail.com>
2024-10-06 19:32:30 -0300

    Sending data in DB

--- src/main/java/com/polianachagas/adoption_project/controller/AnimalController.java
+++ src/main/java/com/polianachagas/adoption_project/controller/AnimalController.java
@@ -1,5 +1,22 @@
 package com.polianachagas.adoption_project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.polianachagas.adoption_project.model.Animal;
+import com.polianachagas.adoption_project.repository.AnimalRepository;
+
+@RestController
 public class AnimalController {
 
+	@Autowired
+	private AnimalRepository animalRepository;
+	
+	@PostMapping("/animals")
+	Animal newAnimal(@RequestBody Animal newAnimal) {
+		return animalRepository.save(newAnimal);
+	}
+	
 }
